e3b83cd1ae3874c13d3fdda8eb3fdac05cb81dc7
chanced <chanceusc@gmail.com>
2020-08-10 16:56:51 -0400

    fighting with vscode/ts imports

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,5 @@
-export const s = new Spike();
+// import { Spike } from '../../core/src/models/spike';
+//                       ↗
+// is suggested by vscode even though even though  ↙ should be correct
+import { Spike } from "@ts-monorepo/core/models/spike";
+export const s = new Spike("sdf");
