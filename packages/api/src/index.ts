d78089bdea800e0e0a7039116b09319a43e85d6c
chanced <chanceusc@gmail.com>
2020-08-10 17:37:31 -0400

    it works

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,5 @@
-// import { Spike } from '../../core/src/models/spike';
-//                       ↗
-// is suggested by vscode even though even though  ↙ should be correct
 import { Spike } from "@ts-monorepo/core/models/spike";
 export const s = new Spike("sdf");
+
+console.log(s.experiment());
+console.log(s);
