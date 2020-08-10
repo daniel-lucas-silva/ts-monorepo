122180dcd05e0f1f72df992978bd9d48456d19b7
chanced <chanceusc@gmail.com>
2020-08-10 19:52:05 -0400

    webpack issues

--- packages/api/webpack.config.js
+++ packages/api/webpack.config.js
@@ -1,36 +1,16 @@
-const path = require("path");
-const TSConfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.ts",
   mode: "development",
-  context: path.resolve(__dirname, "src"),
-  entry: "./index",
   output: {
-    path: path.join(__dirname, "temp"),
     filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\\.tsx?$/,
-        exclude: /^node_modules/,
-        loader: "ts-loader",
-        options: {
-          configFile: "./tsconfig.json",
-        },
-      },
-    ],
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
-    plugins: [
-      new TSConfigPathsPlugin({
-        configFile: "./tsconfig.json",
-        logLevel: "info",
-        extensions: [".ts"],
-        mainFields: ["main"],
-        // baseUrl: "/foo"
-      }),
-    ],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
   },
 };
