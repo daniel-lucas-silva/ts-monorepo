e7ec77d2a76de73adde1f4cd20e08938a29d071e
chanced <chanceusc@gmail.com>
2020-08-11 14:00:17 -0400

    ...shouldnt work

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Spike } from '@ts-monorepo/core'
 export const Home = () => {
-  const s: Spike = new Spike('sdf')
+  const s = new Spike('sdf')
   // const S = new Spike('this fails to compile')
   return <div>{s.message}</div>
 }
